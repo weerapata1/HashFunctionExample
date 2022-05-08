09dcadd96fe175203e44597c49d550a08a31df2b
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2022-05-08 13:28:05 +0700

    hash-SHA-256

--- src/main/java/com/example/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -1,7 +1,13 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Entity.Employee;
+import com.example.demo.Password.Password;
 import com.example.demo.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,6 +16,22 @@ public class EmployeeController {
 
     public EmployeeController(EmployeeRepository employeeRepository){
         this.employeeRepository = employeeRepository;
+    }
+
+    @PostMapping("/empCreate")
+    public ResponseEntity<Employee> empCreate(@RequestParam("name") String name ,@RequestParam("user")String user ,@RequestParam("password")String password){
+
+        Employee employee = new Employee();
+        String message = "";
+        String x = Password.hashSHA256(password);
+
+        employee.setEmpName(name);
+        employee.setEmpUserName(user);
+        employee.setEmpPassword(x);
+        employeeRepository.save(employee);
+
+        message = name + " your account created";
+        return new ResponseEntity(message, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/example/demo/Password/Password.java
+++ src/main/java/com/example/demo/Password/Password.java
@@ -1,5 +1,18 @@
 package com.example.demo.Password;
 
+import lombok.SneakyThrows;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
 public class Password {
+    @SneakyThrows
+    public static String hashSHA256(String originalString){
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(
+                originalString.getBytes(StandardCharsets.UTF_8));
+        return new String(Hex.encode(hash));
+    }
 
 }
